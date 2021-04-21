50e6928f3468f4e52399fc3c4a58c03b8fac0e31
Javier <mrvizious@gmail.com>
2021-04-21 17:50:34 +0200

    EditorController started

---
+++ Assets/Scripts/Controllers/EditorController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorController : MonoBehaviour
+{
+    public TilesList tiles;
+    public GameObject playerPrefab;
+    public GameObject boxPrefab;
+    public GameObject platformPrefab;
+
+
+
+}
